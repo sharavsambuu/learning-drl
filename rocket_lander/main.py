d264517161adc3a61c4feabe2129c8a1766a50d8
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-06-20 09:11:01 +0800

    added some prints

--- rocket_lander/main.py
+++ rocket_lander/main.py
@@ -11,6 +11,9 @@ for i in range(episodes):
 	for _ in range(1000):
 		action = env.action_space.sample()
 		new_state, reward, done, _ = env.step(action)
+		print("Action Taken  ", action)
+		print("Observation   ", state)
+		print("Reward Gained ", reward)
 		state = new_state
 		env.render()
 		if done:
