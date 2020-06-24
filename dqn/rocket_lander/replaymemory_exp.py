007b845edcc4d53aceb920f80192fb560c2bbdda
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-06-25 05:51:41 +0800

    episodic rollouts in order to collect temporatel state frames

--- dqn/rocket_lander/replaymemory_exp.py
+++ dqn/rocket_lander/replaymemory_exp.py
@@ -1,4 +1,5 @@
 from time import sleep
+from collections import deque
 import numpy as np
 import gym
 import cv2
@@ -8,17 +9,17 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-num_steps     = 300
-desired_shape = (140, 180)
 
-env = gym.make('RocketLander-v0')
-env.reset()
+debug_render  = True
 
-print("state shape :", env.observation_space.shape[0])
-print("action size :", env.action_space.shape[0])
-# https://stackoverflow.com/questions/40195740/how-to-run-openai-gym-render-over-a-server/48237220#48237220
-state = env.render(mode='rgb_array')
-print(state.shape)
+num_episodes  = 2
+global_steps  = 0
+
+desired_shape   = (140, 180)
+temporal_frames = deque(maxlen=4)
+
+memory_length = 2000
+replay_memory  = deque(maxlen=memory_length)
 
 def process_frame(frame, shape=(84, 84)):
 	frame = frame.astype(np.uint8)
@@ -28,25 +29,34 @@ def process_frame(frame, shape=(84, 84)):
 	return frame, frame_reshaped
 
 
-gray_state, _ = process_frame(state, shape=desired_shape)
-img = plt.imshow(gray_state, cmap='gray', vmin=0, vmax=255)
-plt.colorbar(img, orientation='horizontal')
-plt.show(block=False)
+env = gym.make('RocketLander-v0')
+env.reset()
 
 
-for _ in range(num_steps):
-	action        = env.action_space.sample()
-	new_state, reward, done, _ = env.step(action)
+for episode in range(num_episodes):
+	print(episode, "is running...")
+	done = False
+	env.reset()
+	# https://stackoverflow.com/questions/40195740/how-to-run-openai-gym-render-over-a-server/48237220#48237220
 	state         = env.render(mode='rgb_array')
 	gray_state, _ = process_frame(state, shape=desired_shape)
+	img           = plt.imshow(gray_state, cmap='gray', vmin=0, vmax=255)
+	plt.colorbar(img, orientation='horizontal')
+	plt.show(block=False)
 
-	# https://www.scivision.dev/fast-update-matplotlib-plots/
-	img.set_data(gray_state)
-	plt.draw()
-	plt.pause(1e-5)
+	while not done:
+		action        = env.action_space.sample()
+		new_state, reward, done, _ = env.step(action)
+		state         = env.render(mode='rgb_array')
+		gray_state, _ = process_frame(state, shape=desired_shape)
+		# https://www.scivision.dev/fast-update-matplotlib-plots/
+		img.set_data(gray_state)
+		plt.draw()
+		plt.pause(1e-5)
 
-	if done==True:
-		break
+		if done==True:
+			plt.clf()
+			print(episode, "is done.")
 
 plt.close("all")
 env.close()
