ddb1e90e1a65d774e048cfe9403e6d53d11d3eaf
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-06-25 06:05:23 +0800

    pre state of saving experience

--- dqn/rocket_lander/replaymemory_exp.py
+++ dqn/rocket_lander/replaymemory_exp.py
@@ -12,16 +12,16 @@ import matplotlib.pyplot as plt
 
 debug_render  = True
 
-num_episodes  = 2
+num_episodes  = 1
 global_steps  = 0
 
 desired_shape   = (140, 180)
-temporal_frames = deque(maxlen=4)
+temporal_frames = deque(maxlen=4+1)
 
 memory_length = 2000
 replay_memory  = deque(maxlen=memory_length)
 
-def process_frame(frame, shape=(84, 84)):
+def preprocess_frame(frame, shape=(84, 84)):
 	frame = frame.astype(np.uint8)
 	frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 	frame = cv2.resize(frame, shape, interpolation=cv2.INTER_NEAREST)
@@ -34,23 +34,27 @@ env.reset()
 
 
 for episode in range(num_episodes):
-	print(episode, "is running...")
-	done = False
 	env.reset()
-	# https://stackoverflow.com/questions/40195740/how-to-run-openai-gym-render-over-a-server/48237220#48237220
-	state         = env.render(mode='rgb_array')
-	gray_state, _ = process_frame(state, shape=desired_shape)
-	img           = plt.imshow(gray_state, cmap='gray', vmin=0, vmax=255)
+	print(episode, "is running...")
+	done     = False
+	state    = env.render(mode='rgb_array')
+	state, _ = preprocess_frame(state, shape=desired_shape)
+
+	img               = plt.imshow(state, cmap='gray', vmin=0, vmax=255)
 	plt.colorbar(img, orientation='horizontal')
 	plt.show(block=False)
 
 	while not done:
-		action        = env.action_space.sample()
-		new_state, reward, done, _ = env.step(action)
-		state         = env.render(mode='rgb_array')
-		gray_state, _ = process_frame(state, shape=desired_shape)
-		# https://www.scivision.dev/fast-update-matplotlib-plots/
-		img.set_data(gray_state)
+		state    = env.render(mode='rgb_array')
+		state, _ = preprocess_frame(state, shape=desired_shape)
+
+		action             = env.action_space.sample()
+		_, reward, done, _ = env.step(action)
+
+		new_state    = env.render(mode='rgb_array')
+		new_state, _ = preprocess_frame(new_state, shape=desired_shape)
+		#print(new_state.shape)
+		img.set_data(new_state)
 		plt.draw()
 		plt.pause(1e-5)
 
