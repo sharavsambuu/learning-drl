57c7f00a6b28aa220223edad0b44cf68e2f699a5
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-06-25 04:56:57 +0800

    preprocessing frame is in progress

--- dqn/rocket_lander/preprocess_state_exp.py
+++ dqn/rocket_lander/preprocess_state_exp.py
@@ -8,24 +8,17 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-env      = gym.make('RocketLander-v0')
+num_steps     = 300
+desired_shape = (140, 180)
+
+env = gym.make('RocketLander-v0')
 env.reset()
 
-sleep(1)
 print("state shape :", env.observation_space.shape[0])
 print("action size :", env.action_space.shape[0])
 # https://stackoverflow.com/questions/40195740/how-to-run-openai-gym-render-over-a-server/48237220#48237220
 state = env.render(mode='rgb_array')
 print(state.shape)
-
-# rendering original numpy array (700, 504, 3)
-#plt.figure(figsize=(240, 240))
-img = plt.imshow(state)
-plt.colorbar(img, orientation='horizontal')
-plt.show(block=False)
-plt.pause(2)
-plt.close()
-
 
 def process_frame(frame, shape=(84, 84)):
 	frame = frame.astype(np.uint8)
@@ -34,27 +27,26 @@ def process_frame(frame, shape=(84, 84)):
 	frame_reshaped = frame.reshape((*shape, 1))
 	return frame, frame_reshaped
 
-gray_state, _ = process_frame(state, shape=(state.shape[1], state.shape[0]))
-#print(gray_state.shape)
+
+gray_state, _ = process_frame(state, shape=desired_shape)
 img = plt.imshow(gray_state, cmap='gray', vmin=0, vmax=255)
 plt.colorbar(img, orientation='horizontal')
 plt.show(block=False)
 
-for _ in range(1000):
+
+for _ in range(num_steps):
 	action        = env.action_space.sample()
 	new_state, reward, done, _ = env.step(action)
 	state         = env.render(mode='rgb_array')
-	#gray_state, _ = process_frame(state, shape=(state.shape[1], state.shape[0]))
-	gray_state, _ = process_frame(state, shape=(240, 240))
-	print(gray_state.shape)
+	gray_state, _ = process_frame(state, shape=desired_shape)
 
 	# https://www.scivision.dev/fast-update-matplotlib-plots/
 	img.set_data(gray_state)
 	plt.draw()
 	plt.pause(1e-5)
 
-	if done==True:
-		break
+	#if done==True:
+	#	break
 
 plt.close("all")
 env.close()
