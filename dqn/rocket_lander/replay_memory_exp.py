6402976ad77211f25a90a39063d01866945fd550
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-06-25 10:21:32 +0800

    stacked and reshaped temporal frames into one tensor in order to feed to the neural network

--- dqn/rocket_lander/replay_memory_exp.py
+++ dqn/rocket_lander/replay_memory_exp.py
@@ -52,17 +52,41 @@ for episode in range(num_episodes):
 		action             = env.action_space.sample()
 		_, reward, done, _ = env.step(action)
 
-		new_state    = env.render(mode='rgb_array')
-		new_state, _ = preprocess_frame(new_state, shape=desired_shape)
+		new_state                     = env.render(mode='rgb_array')
+		new_state, new_state_reshaped = preprocess_frame(new_state, shape=desired_shape)
 
 		img.set_data(new_state)
 		plt.draw()
 		plt.pause(1e-5)
 
-		temporal_frames.append(new_state)
+		temporal_frames.append(new_state_reshaped)
 		if len(temporal_frames)==5:
 			prev_state = list(temporal_frames)[:temporal_length]
 			next_state = list(temporal_frames)[1:]
+
+			# Дараалсан фрэймүүдийг нэгтгээд нэг тензор болгох. 
+			# Неорон модельрүү чихэхэд амар 
+			# Тензорын дүрс нь (өндөр, өргөн, фрэймийн тоо)
+			prev_state = np.stack(prev_state, axis=-1)
+			prev_state = np.reshape(
+					prev_state, 
+					(
+						prev_state.shape[ 0], 
+						prev_state.shape[ 1], 
+						prev_state.shape[-1]
+					)
+				)
+			next_state = np.stack(next_state, axis=-1)
+			next_state = np.reshape(
+					next_state,
+					(
+						next_state.shape[ 0],
+						next_state.shape[ 1],
+						next_state.shape[-1]
+					)
+				)
+			print(prev_state.shape, next_state.shape)
+			
 			replay_memory.append((prev_state, action, reward, next_state, done))
 			pass
 
