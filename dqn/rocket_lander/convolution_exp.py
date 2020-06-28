0699c8e650d69fc217049060947914e23d6b6f1d
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-06-28 08:40:58 +0800

    initial convolutional Q network design

--- dqn/rocket_lander/convolution_exp.py
+++ dqn/rocket_lander/convolution_exp.py
@@ -12,8 +12,48 @@ print(d2.shape)
 batch = np.array([d1, d2])
 print(batch.shape)
 
-conv_layer = tf.keras.layers.Conv2D(32, (8,8), strides=4, activation='relu')
+# эхний convolutional давхарга
+conv_layer1 = tf.keras.layers.Conv2D(32, (8,8), strides=4, activation='relu')
+conv_out1   = conv_layer1(batch)
+print("эхний convolutional давхаргын гаралтын дүрс")
+print(conv_out1.shape)
 
-conv_out   = conv_layer(batch)
-print(conv_out.shape)
+# maxpool давхарга
+maxpool_layer1 = tf.keras.layers.MaxPooling2D((2,2), strides=2)
+maxpool_out1   = maxpool_layer1(conv_out1)
+print("эхний maxpool давхаргын гаралтын дүрс")
+print(maxpool_out1.shape)
 
+# хоёр дахь convolutional давхарга
+conv_layer2 = tf.keras.layers.Conv2D(64, (4,4), strides=1, activation='relu')
+conv_out2   = conv_layer2(maxpool_out1)
+print("хоёр дахь convolutional давхаргын гаралтын дүрс")
+print(conv_out2.shape)
+
+# хоёр дахь maxpool давхарга
+maxpool_layer2 = tf.keras.layers.MaxPooling2D((2,2), strides=2)
+maxpool_out2   = maxpool_layer2(conv_out2)
+print("хоёр дахь maxpool давхаргын гаралт")
+print(maxpool_out2.shape)
+
+
+# гурав дахь convolutional давхарга
+conv_layer3 = tf.keras.layers.Conv2D(1024, (7,7), strides=1, activation='relu')
+conv_out3   = conv_layer3(maxpool_out2)
+print("гурав дахь convolutional гаралтын дүрс")
+print(conv_out3.shape)
+
+# сүүлийн давхаргаруу бэлтгэх үе шат, бүх неоронуудыг нэг векторлуу жагсаах
+flatten_layer = tf.keras.layers.Flatten()
+flatten_out   = flatten_layer(conv_out3)
+print("flatten давхаргын гаралтын дүрс")
+print(flatten_out.shape)
+
+# үйлдлийн төрлийн тоог 3-н ширхэг үзье
+n_actions = 3
+
+# гаралтын давхарга
+output_layer = tf.keras.layers.Dense(n_actions, activation='softmax')
+last_output  = output_layer(flatten_out)
+print("сүүлийн гаралтын давхаргын дүрс")
+print(last_output.shape)
