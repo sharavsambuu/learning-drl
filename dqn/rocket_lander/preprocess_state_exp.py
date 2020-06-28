af8438d7864d73902e804fa77c47086a53dfbee8
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-06-28 15:26:33 +0800

    converted tabs top spaces

--- dqn/rocket_lander/preprocess_state_exp.py
+++ dqn/rocket_lander/preprocess_state_exp.py
@@ -15,17 +15,18 @@ env = gym.make('RocketLander-v0')
 env.reset()
 
 print("state shape :", env.observation_space.shape[0])
-print("action size :", env.action_space.shape[0])
+#print("action size :", env.action_space.shape[0])
+print("action size :", env.action_space.n)
 # https://stackoverflow.com/questions/40195740/how-to-run-openai-gym-render-over-a-server/48237220#48237220
 state = env.render(mode='rgb_array')
 print(state.shape)
 
 def process_frame(frame, shape=(84, 84)):
-	frame = frame.astype(np.uint8)
-	frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-	frame = cv2.resize(frame, shape, interpolation=cv2.INTER_NEAREST)
-	frame_reshaped = frame.reshape((*shape, 1))
-	return frame, frame_reshaped
+  frame = frame.astype(np.uint8)
+  frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+  frame = cv2.resize(frame, shape, interpolation=cv2.INTER_NEAREST)
+  frame_reshaped = frame.reshape((*shape, 1))
+  return frame, frame_reshaped
 
 
 gray_state, _ = process_frame(state, shape=desired_shape)
@@ -35,18 +36,18 @@ plt.show(block=False)
 
 
 for _ in range(num_steps):
-	action        = env.action_space.sample()
-	new_state, reward, done, _ = env.step(action)
-	state         = env.render(mode='rgb_array')
-	gray_state, _ = process_frame(state, shape=desired_shape)
+  action        = env.action_space.sample()
+  new_state, reward, done, _ = env.step(action)
+  state         = env.render(mode='rgb_array')
+  gray_state, _ = process_frame(state, shape=desired_shape)
 
-	# https://www.scivision.dev/fast-update-matplotlib-plots/
-	img.set_data(gray_state)
-	plt.draw()
-	plt.pause(1e-5)
+  # https://www.scivision.dev/fast-update-matplotlib-plots/
+  img.set_data(gray_state)
+  plt.draw()
+  plt.pause(1e-5)
 
-	if done==True:
-		break
+  if done==True:
+    break
 
 plt.close("all")
 env.close()
--- dqn/rocket_lander/random_walk_exp.py
+++ dqn/rocket_lander/random_walk_exp.py
@@ -7,16 +7,16 @@ env = gym.make('RocketLander-v0')
 episodes = 5
 
 for i in range(episodes):
-	state = env.reset()
-	for _ in range(1000):
-		action = env.action_space.sample()
-		new_state, reward, done, _ = env.step(action)
-		print("Action Taken  ", action)
-		print("Observation   ", state)
-		print("Reward Gained ", reward)
-		state = new_state
-		env.render()
-		if done:
-			break
+  state = env.reset()
+  for _ in range(1000):
+    action = env.action_space.sample()
+    new_state, reward, done, _ = env.step(action)
+    print("Action Taken  ", action)
+    print("Observation   ", state)
+    print("Reward Gained ", reward)
+    state = new_state
+    env.render()
+    if done:
+      break
 
 env.close()
