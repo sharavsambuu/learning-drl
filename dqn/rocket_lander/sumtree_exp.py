c1a7da79fba79f044cac04bc5147e8308b608e3b
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-07-05 20:42:30 +0800

    sumtree for PER from the internet

--- dqn/rocket_lander/sumtree_exp.py
+++ dqn/rocket_lander/sumtree_exp.py
@@ -1,3 +1,4 @@
+import random
 import numpy
 
 # https://jaromiru.com/2016/11/07/lets-make-a-dqn-double-learning-and-prioritized-experience-replay
@@ -5,15 +6,15 @@ class SumTree:
     write = 0
     def __init__(self, capacity):
         self.capacity = capacity
-        self.tree = numpy.zeros( 2*capacity - 1 )
-        self.data = numpy.zeros( capacity, dtype=object )
+        self.tree     = numpy.zeros( 2*capacity - 1 )
+        self.data     = numpy.zeros( capacity, dtype=object )
     def _propagate(self, idx, change):
-        parent = (idx - 1) // 2
+        parent             = (idx - 1) // 2
         self.tree[parent] += change
         if parent != 0:
             self._propagate(parent, change)
     def _retrieve(self, idx, s):
-        left = 2 * idx + 1
+        left  = 2 * idx + 1
         right = left + 1
         if left >= len(self.tree):
             return idx
@@ -24,19 +25,42 @@ class SumTree:
     def total(self):
         return self.tree[0]
     def add(self, p, data):
-        idx = self.write + self.capacity - 1
+        idx                   = self.write + self.capacity - 1
         self.data[self.write] = data
         self.update(idx, p)
-        self.write += 1
+        self.write           += 1
         if self.write >= self.capacity:
             self.write = 0
     def update(self, idx, p):
-        change = p - self.tree[idx]
+        change         = p - self.tree[idx]
         self.tree[idx] = p
         self._propagate(idx, change)
     def get(self, s):
-        idx = self._retrieve(0, s)
+        idx     = self._retrieve(0, s)
         dataIdx = idx - self.capacity + 1
         return (idx, self.tree[idx], self.data[dataIdx])
+
+
+tree = SumTree(1000)
+
+tree.add(10.0, ( 1, 2, 3))
+tree.add(1.0 , ( 2, 3, 4))
+tree.add(11.0, ( 5, 6, 7))
+tree.add(0.0 , ( 8, 9,10))
+tree.add(2.0 , (11,12,13))
+tree.add(12.0, (14,15,16))
+
+
+
+batch_size = 5
+batch      = []
+
+segment    = tree.total() / batch_size
+for i in range(batch_size):
+    a = segment * i
+    b = segment * (i + 1)
+    s = random.uniform(a, b)
+    (idx, p, data) = tree.get(s)
+    print(idx, p, data)
 
 
