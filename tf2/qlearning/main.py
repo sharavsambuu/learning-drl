4a88179b9e42be552374870e876fbb0ad266be69
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-07-25 01:10:57 +0800

    fixed tab spaces

--- tf2/qlearning/main.py
+++ tf2/qlearning/main.py
@@ -24,27 +24,27 @@ episodes      = 4000
 reward_list   = []
 
 for i in range(episodes):
-  # episode эхлэж байгаа тул environment тэр чигт нь шинэчлэх
-  state        = env.reset()
-  total_reward = 0
-  done         = False
-  while True:
-    # action буюу үйлдлийг сонгох, гэхдээ тодорхой хэмжээний noise-тойгоор
-    action = np.argmax(Q[state, :]+np.random.randn(1, env.action_space.n)*(1.0/(i+1)))
-    # үйлдэл хийж шинэ төлөв болон reward оноог авах
-    new_state, new_reward, done, _ = env.step(action)
-    # шинэ олж авсан мэдлэгээрээ Q хүснэгтийг шинэчлэх
-    Q[state, action] = Q[state, action] + \
-        learning_rate*(new_reward + gamma*np.max(Q[new_state, :]) - Q[state, action])
-    total_reward     = total_reward + new_reward
-    state            = new_state
+    # episode эхлэж байгаа тул environment тэр чигт нь шинэчлэх
+    state        = env.reset()
+    total_reward = 0
+    done         = False
+    while True:
+        # action буюу үйлдлийг сонгох, гэхдээ тодорхой хэмжээний noise-тойгоор
+        action = np.argmax(Q[state, :]+np.random.randn(1, env.action_space.n)*(1.0/(i+1)))
+        # үйлдэл хийж шинэ төлөв болон reward оноог авах
+        new_state, new_reward, done, _ = env.step(action)
+        # шинэ олж авсан мэдлэгээрээ Q хүснэгтийг шинэчлэх
+        Q[state, action] = Q[state, action] + \
+            learning_rate*(new_reward + gamma*np.max(Q[new_state, :]) - Q[state, action])
+        total_reward     = total_reward + new_reward
+        state            = new_state
 
-    #system("clear")
-    env.render()
+        #system("clear")
+        env.render()
 
-    if done == True:
-      break
-  reward_list.append(total_reward)
+        if done == True:
+            break
+    reward_list.append(total_reward)
 
 print("Сургаж дууслаа.")
 
