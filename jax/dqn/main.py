269278a61e5dacc15a2d31892835dbdac44945e5
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-07-25 21:41:23 +0800

    created basic flax dqn model

--- jax/dqn/main.py
+++ jax/dqn/main.py
@@ -4,17 +4,38 @@ import math
 import gym
 from collections import deque
 
+import flax
+import jax
 
 debug_render  = True
 num_episodes  = 100
 memory_length = 4000
+batch_size    = 64
+learning_rate = 0.001
 replay_memory = deque(maxlen=memory_length)
 
 
-env = gym.make('CartPole-v0')
-env.reset()
+class DeepQNetwork(flax.nn.Module):
+    def apply(self, x, n_actions):
+        dense_layer_1      = flax.nn.Dense(x, 64)
+        activation_layer_1 = flax.nn.relu(dense_layer_1)
+        dense_layer_2      = flax.nn.Dense(activation_layer_1, 32)
+        activation_layer_2 = flax.nn.relu(dense_layer_2)
+        output_layer       = flax.nn.Dense(activation_layer_2, n_actions)
+        return output_layer
 
-n_actions = env.action_space.n
+
+env   = gym.make('CartPole-v0')
+state = env.reset()
+
+n_actions        = env.action_space.n
+
+dqn_module       = DeepQNetwork.partial(n_actions=n_actions)
+_, params        = dqn_module.init_by_shape(jax.random.PRNGKey(0), [state.shape])
+q_network        = flax.nn.Model(dqn_module, params)
+target_q_network = flax.nn.Model(dqn_module, params)
+
+optimizer        = flax.optim.Adam(learning_rate).create(q_network)
 
 
 global_steps = 0
@@ -25,6 +46,7 @@ try:
             global_steps = global_steps+1
             action       = env.action_space.sample()
             new_state, reward, done, _ = env.step(action)
+            print(new_state.shape)
             if done:
                 new_state = None
             replay_memory.append((state, action, reward, new_state))
@@ -36,6 +58,5 @@ try:
                 env.render()
             if done:
                 break
-            print(len(replay_memory))
 finally:
     env.close()
