fdaf3dad8bef6b0610a9f7b7de9b268461eb34cc
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2020-07-30 00:49:21 +0800

    failed attempt to try A3C with ray library through parameter sharing

--- jax/a3c/multiprocessing_a3c.py
+++ jax/a3c/multiprocessing_a3c.py
@@ -102,17 +102,28 @@ def backpropagate_actor(optimizer, critic_model, props):
     return optimizer, loss
 
 
-
-actor_module     = ActorNetwork.partial(n_actions=n_actions)
-_, actor_params  = actor_module.init_by_shape(jax.random.PRNGKey(0), [state.shape])
-critic_module    = CriticNetwork.partial()
-_, critic_params = critic_module.init_by_shape(jax.random.PRNGKey(0), [state.shape])
-
-
 @ray.remote
 def training_worker(parameter_server):
-    actor_params, critic_params = parameter_server.get.remote(
-                        ['actor_params', 'critic_params'])
+    # temporary solution to cope with serialization issue
+    class ActorNetwork(flax.nn.Module):
+        def apply(self, x, n_actions):
+            dense_layer_1      = flax.nn.Dense(x, 64)
+            activation_layer_1 = flax.nn.relu(dense_layer_1)
+            dense_layer_2      = flax.nn.Dense(activation_layer_1, 32)
+            activation_layer_2 = flax.nn.relu(dense_layer_2)
+            output_dense_layer = flax.nn.Dense(activation_layer_2, n_actions)
+            output_layer       = flax.nn.softmax(output_dense_layer)
+            return output_layer
+    class CriticNetwork(flax.nn.Module):
+        def apply(self, x):
+            dense_layer_1      = flax.nn.Dense(x, 64)
+            activation_layer_1 = flax.nn.relu(dense_layer_1)
+            dense_layer_2      = flax.nn.Dense(activation_layer_1, 32)
+            activation_layer_2 = flax.nn.relu(dense_layer_2)
+            output_dense_layer = flax.nn.Dense(activation_layer_2, 1)
+            return output_dense_layer
+    actor_params, critic_params = ray.get(parameter_server.get.remote(
+                        ['actor_params', 'critic_params']))
     actor_module     = ActorNetwork.partial(n_actions=n_actions)
     critic_module    = CriticNetwork.partial()
     actor_model      = flax.nn.Model(actor_module, actor_params)
@@ -127,20 +138,24 @@ def training_worker(parameter_server):
             state = env.reset()
             states, actions, rewards, dones = [], [], [], []
             while True:
-                actor_params, critic_params = parameter_server.get.remote(
-                        ['actor_params', 'critic_params'])
-                actor_model  = actor_model.replace(params=actor_params)
+                actor_params, critic_params = ray.get(parameter_server.get.remote(
+                        ['actor_params', 'critic_params']))
+                actor_model  = actor_model .replace(params=actor_params)
                 critic_model = critic_model.replace(params=critic_params)
 
                 action_probabilities  = actor_inference(actor_optimizer.target, jnp.asarray([state]))
                 action_probabilities  = np.array(action_probabilities[0])
                 action                = np.random.choice(n_actions, p=action_probabilities)
+
                 next_state, reward, done, _ = env.step(int(action))
+
                 states.append(state)
                 actions.append(action)
                 rewards.append(reward)
                 dones.append(int(done))
+
                 state = next_state
+
                 if done:
                     print("episode {}, reward : {}".format(episode, sum(rewards)))
                     episode_length = len(rewards)
@@ -150,8 +165,9 @@ def training_worker(parameter_server):
                         for idx, j in enumerate(range(t, episode_length)):
                             G_t = G_t + (gamma**idx)*rewards[j]*(1-dones[j])
                         discounted_rewards[t] = G_t
-                    discounted_rewards = discounted_rewards - np.mean(discounted_rewards)
-                    discounted_rewards = discounted_rewards / (np.std(discounted_rewards)+1e-10)
+                    discounted_rewards  = discounted_rewards - np.mean(discounted_rewards)
+                    discounted_rewards  = discounted_rewards / (np.std(discounted_rewards)+1e-10)
+
                     actor_optimizer, _  = backpropagate_actor(
                         actor_optimizer,
                         critic_optimizer.target,
@@ -168,17 +184,18 @@ def training_worker(parameter_server):
                             jnp.asarray(discounted_rewards),
                         )
                     )
-                    parameter_server.update.remote(
-                            ['actor_params', 'critic_params'],
-                            [
-                                actor_optimizer.target.params,
-                                critic_optimizer.target.params
-                            ])
+                    #parameter_server.update.remote(
+                    #        ['actor_params', 'critic_params'],
+                    #        [
+                    #            actor_optimizer.target.params,
+                    #            critic_optimizer.target.params
+                    #        ])
                     episode_count = episode_count + 1
                     break
     finally:
         env.close()
-
+        return 0
+    return 0
 
 @ray.remote
 class ParameterServer(object):
@@ -190,22 +207,14 @@ class ParameterServer(object):
         for key, value in zip(keys, values):
             self.parameters[key] = value
 
+actor_module     = ActorNetwork.partial(n_actions=n_actions)
+_, actor_params  = actor_module.init_by_shape (jax.random.PRNGKey(0), [state.shape])
+critic_module    = CriticNetwork.partial()
+_, critic_params = critic_module.init_by_shape(jax.random.PRNGKey(0), [state.shape])
+
 parameter_server = ParameterServer.remote(
         ['actor_params', 'critic_params'],
         [actor_params, critic_params])
-result = parameter_server.get.remote(
-        ['actor_params', 'critic_params'])
-print("got initial result")
-parameter_server.update.remote(
-        ['actor_params', 'critic_params'],
-        [actor_params, critic_params])
-print("parameter updated...")
-result = parameter_server.get.remote(
-        ['actor_params', 'critic_params'])
-ray.get(result)
-print("got updated parameters")
-print(ray.get(result))
-exit(0)
 
 
 for _ in range(n_workers):
