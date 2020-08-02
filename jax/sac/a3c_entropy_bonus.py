fix entropy calculation on a2c and a3c with ent-bonus

jax/sac/a3c_entropy_bonus.py
@@ -67,8 +67,8 @@ def backpropagate_actor(optimizer, critic_model, props):
         action_probabilities      = model(props[0])
         probabilities             = gather(action_probabilities, props[2])
         log_probabilities         = -jnp.log(probabilities)
-        alpha                     = 0.05 # Entropy temperature
-        entropies                 = -jnp.log(probabilities)*alpha
+        alpha                     = 0.8 # Entropy temperature
+        entropies                 = jnp.multiply(probabilities, jnp.log(probabilities))*alpha
         advantages_with_entropies = jnp.add(advantages, entropies)
         return jnp.mean(jnp.multiply(log_probabilities, advantages_with_entropies))
     loss, gradients = jax.value_and_grad(loss_fn)(optimizer.target)