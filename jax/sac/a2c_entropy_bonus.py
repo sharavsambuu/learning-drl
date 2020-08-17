debugging shape transitions in a2c with entropy bonus, fixed wrong shape

jax/sac/a2c_entropy_bonus.py
@@ -94,14 +94,33 @@ def backpropagate_actor(optimizer, critic_model, props):
     # props[1] - discounted_rewards
     # props[2] - actions
     values      = jax.lax.stop_gradient(critic_model(props[0]))
-    advantages  = props[1] - values
+    values      = jnp.reshape(values, (values.shape[0],))
+    print("critic values shape:", values.shape)
+    print("discounted rewards shape:", props[1].shape)
+    advantages  = jnp.subtract(discounted_rewards, values)
     def loss_fn(model):
         action_probabilities      = model(props[0])
+        # should be (batch, action_dim)
+        print("action probabilities shape:", action_probabilities.shape)
         probabilities             = gather(action_probabilities, props[2])
+        # should be (batch,)
+        print("probabilities gather by action shape:", probabilities.shape)
         log_probabilities         = -jnp.log(probabilities)
-        alpha                     = 0.8 # Entropy temperature
-        entropies                 = jnp.multiply(probabilities, jnp.log(probabilities))*alpha
+        # should be (batch)
+        print("log probabilities shape:", log_probabilities.shape)
+        alpha                     = 0.4 # Entropy temperature
+
+        entropies                 = -jnp.sum(
+                    jnp.multiply(
+                        action_probabilities,
+                        jnp.log(action_probabilities)
+                    ),
+                    axis = 1
+                )*alpha
+        print("advantages shape:", advantages.shape)
+        print("entropies shape:", entropies.shape)
         advantages_with_entropies = jnp.add(advantages, entropies)
+        print("advantages with entropies shape:", advantages_with_entropies.shape)
         return jnp.mean(jnp.multiply(log_probabilities, advantages_with_entropies))
     loss, gradients = jax.value_and_grad(loss_fn)(optimizer.target)
     optimizer       = optimizer.apply_gradient(gradients)
@@ -146,6 +165,7 @@ try:
                     discounted_rewards[t] = G_t
                 discounted_rewards = discounted_rewards - np.mean(discounted_rewards)
                 discounted_rewards = discounted_rewards / (np.std(discounted_rewards)+1e-10)
+                print("pre discounted rewards shape:::", discounted_rewards.shape)
 
                 actor_optimizer, _  = backpropagate_actor(
                     actor_optimizer,