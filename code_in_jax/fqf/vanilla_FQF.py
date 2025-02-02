a6b337315d29c32cc675e8381b80d92cdc857214
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2025-02-02 10:04:18 +0800

    FQF update.

--- code_in_jax/fqf/vanilla_FQF.py
+++ code_in_jax/fqf/vanilla_FQF.py
@@ -3,7 +3,6 @@ import random
 import math
 import gymnasium as gym
 from collections import deque
-
 import flax.linen as nn
 import jax
 from jax import jit, vmap
@@ -80,7 +79,7 @@ class PERMemory:
 
     def __init__(self, capacity):
         self.tree = SumTree(capacity)
-        self.capacity = capacity # Added capacity to PERMemory
+        self.capacity = capacity
 
     def _get_priority(self, error):
         return (error + self.e) ** self.a
@@ -97,9 +96,7 @@ class PERMemory:
             b = segment * (i + 1)
             s = random.uniform(a, b)
             (idx, p, data) = self.tree.get(s)
-            assert isinstance(data, tuple), f"Data is not a tuple, but {type(data)}" # Assertion to check data type
             batch.append((idx, data))
-        assert len(batch) == n, f"Batch size is not equal to n, batch_size={len(batch)}, n={n}" # Assertion to check batch size
         return batch
 
     def update(self, idx, error):
@@ -108,82 +105,71 @@ class PERMemory:
 
 
 
-# FQF Network
 class FQFNetwork(nn.Module):
-    n_actions: int
-    n_quantiles: int
+    n_actions    : int
+    n_quantiles  : int
     n_tau_samples: int
 
     @nn.compact
-    def __call__(self, x):
-        # State embedding
+    def __call__(self, x, tau_hats):
         x = nn.Dense(features=hidden_size)(x)
         x = nn.relu(x)
         x = nn.Dense(features=hidden_size // 2)(x)
         state_embedding = nn.relu(x)
 
-        # Fraction proposal network
-        fraction_layer = nn.Dense(features=self.n_quantiles)(state_embedding)
+        fraction_layer  = nn.Dense(features=self.n_quantiles)(state_embedding)
         fraction_logits = nn.relu(fraction_layer)
-        tau_hats = nn.softmax(fraction_logits, axis=-1)
-        tau_hats = jnp.cumsum(tau_hats, axis=-1)
 
-        # generate tau_i
         tau_0 = jnp.zeros((x.shape[0], 1))
         tau_hats = jnp.concatenate([tau_0, tau_hats], axis=-1)
 
         tau_i = (tau_hats[:, 1:] + tau_hats[:, :-1]) / 2.
         tau_i = jnp.tile(tau_i[:, jnp.newaxis, :], (1, self.n_actions, 1))
 
-        # Cosine embedding of tau_i
         i_pi = jnp.arange(1, self.n_tau_samples + 1, dtype=jnp.float32) * jnp.pi
-        tau_i_embedding = jnp.cos(tau_i[..., jnp.newaxis] * i_pi)
+        tau_i_embedding = jnp.cos(jnp.expand_dims(tau_i + 1e-8, axis=-1) * i_pi)
         tau_i_embedding = tau_i_embedding.reshape(x.shape[0], self.n_actions, -1)
 
-        # Value network
-        # Concatenate state embedding with cosine embedding
         state_embedding_expanded = jnp.tile(state_embedding[:, jnp.newaxis, :], (1, self.n_actions, 1))
         x = jnp.concatenate([state_embedding_expanded, tau_i_embedding], axis=-1)
         x = nn.Dense(features=hidden_size)(x)
         x = nn.relu(x)
         quantile_values = nn.Dense(features=self.n_quantiles)(x)
 
-        return quantile_values, tau_hats
+        return quantile_values, fraction_logits, tau_hats
 
 
-# Create environment
 env = gym.make('CartPole-v1', render_mode="human")
 state, info = env.reset()
 
-# Instantiate network and optimizer
+
 n_actions   = env.action_space.n
 fqf_module  = FQFNetwork(n_actions=n_actions, n_quantiles=n_quantiles, n_tau_samples=n_tau_samples)
 dummy_state = jnp.zeros_like(state)
-params                  = fqf_module.init(jax.random.PRNGKey(0), jnp.expand_dims(dummy_state, axis=0))
+params                  = fqf_module.init(jax.random.PRNGKey(0), jnp.expand_dims(dummy_state, axis=0), jnp.zeros((1, n_quantiles)))
 q_network_params        = params['params']
 target_q_network_params = params['params']
 
 optimizer = optax.adam(learning_rate)
 opt_state = optimizer.init(q_network_params)
 
-# PER Memory
 per_memory = PERMemory(memory_length)
+
 
 def policy(params, x):
     x = jnp.expand_dims(x, axis=0)
-    predicted_q_values, _ = fqf_module.apply({'params': params}, x)
+    predicted_q_values, fraction_logits, tau_hats = fqf_module.apply({'params': params}, x, tau_hats=jnp.zeros((1, n_quantiles)))
     expected_q_values = jnp.mean(predicted_q_values, axis=-1)
     max_q_action = jnp.argmax(expected_q_values, axis=-1)
     return max_q_action[0], expected_q_values[0]
 
 @jit
-def calculate_quantile_values(params, x, n_quantiles_sample):
-    quantile_values, tau_hats = fqf_module.apply({'params': params}, x)
+def calculate_quantile_values(params, x, tau_hats):
+    quantile_values, _, _ = fqf_module.apply({'params': params}, x, tau_hats)
     return quantile_values, tau_hats
 
-# Huber loss
 @jit
-def huber_loss(td_errors, tau_i):
+def quantile_huber_loss(td_errors, tau_i):
     delta = 1.0
     abs_td_errors = jnp.abs(td_errors)
     quadratic = jnp.minimum(abs_td_errors, delta)
@@ -191,25 +177,31 @@ def huber_loss(td_errors, tau_i):
     loss = 0.5 * quadratic ** 2 + delta * linear
     tau_i_expanded = jnp.expand_dims(tau_i, axis=-1)
     quantile_huber_loss = jnp.abs(tau_i_expanded - (td_errors < 0.0).astype(jnp.float32)) * loss
-    return jnp.mean(jnp.sum(quantile_huber_loss, axis=1))
+    return jnp.mean(jnp.sum(quantile_huber_loss, axis=-1))
 
 @jit
-def train_step(q_network_params, target_q_network_params, opt_state, batch, key):
+def train_step(q_network_params, target_q_network_params, opt_state, batch):
     def loss_fn(params):
-        predicted_quantile_values, tau_hats = fqf_module.apply({'params': params}, batch[0])
-        target_quantile_values, _ = fqf_module.apply({'params': target_q_network_params}, batch[3])
+        predicted_quantile_values, fraction_logits, current_tau_hats = fqf_module.apply({'params': params}, batch[0], tau_hats=jnp.zeros((batch_size, n_quantiles)))
+        target_quantile_values_next, fraction_logits_next, tau_hats_next = fqf_module.apply({'params': target_q_network_params}, batch[3], tau_hats=jnp.zeros((batch_size, n_quantiles)))
 
-        tau_i = (tau_hats[:, 1:] + tau_hats[:, :-1]) / 2.
+        tau_hats = nn.softmax(fraction_logits, axis=-1)
+        tau_hats = jnp.cumsum(tau_hats, axis=-1)
+
+        tau_hats_next = nn.softmax(fraction_logits_next, axis=-1)
+        tau_hats_next = jnp.cumsum(tau_hats_next, axis=-1)
+
+        tau_i = (current_tau_hats[:, 1:] + current_tau_hats[:, :-1]) / 2.
         tau_i_selected = tau_i[jnp.arange(batch_size), batch[1]]
 
-        expected_next_q_values = jnp.mean(target_quantile_values, axis=2)
+        expected_next_q_values = jnp.mean(target_quantile_values_next, axis=2)
         max_next_actions = jnp.argmax(expected_next_q_values, axis=1)
-        next_state_quantile_values = target_quantile_values[jnp.arange(batch_size), max_next_actions]
+        next_state_quantile_values = target_quantile_values_next[jnp.arange(batch_size), max_next_actions]
 
         target_quantile_values = batch[2][:, jnp.newaxis] + gamma * (1 - batch[4][:, jnp.newaxis]) * next_state_quantile_values
         predicted_action_quantile_values = predicted_quantile_values[jnp.arange(batch_size), batch[1]]
         td_errors = target_quantile_values - predicted_action_quantile_values
-        loss = huber_loss(td_errors, tau_i_selected)
+        loss = quantile_huber_loss(td_errors, tau_i_selected)
         return loss, td_errors
 
     (loss, td_errors), gradients = jax.value_and_grad(loss_fn, has_aux=True)(q_network_params)
@@ -217,6 +209,8 @@ def train_step(q_network_params, target_q_network_params, opt_state, batch, key)
     q_network_params = optax.apply_updates(q_network_params, updates)
     new_priorities = jnp.abs(jnp.mean(td_errors, axis=1))
     return q_network_params, opt_state, loss, new_priorities
+
+
 
 rng = jax.random.PRNGKey(0)
 global_steps = 0
@@ -242,18 +236,18 @@ try:
             done = terminated or truncated
             new_state = np.array(new_state, dtype=np.float32)
 
-            per_memory.add(0, (state, action, reward, new_state, float(done)))
+            per_memory.add(1e9, (state, action, reward, new_state, float(done)))
 
             if len(per_memory.tree.data) >= batch_size:
                 batch = per_memory.sample(batch_size)
                 states, actions, rewards, next_states, dones = [], [], [], [], []
                 for i in range(batch_size):
                     data = batch[i][1]
-                    states.append(data[0])
-                    actions.append(data[1])
-                    rewards.append(data[2])
+                    states     .append(data[0])
+                    actions    .append(data[1])
+                    rewards    .append(data[2])
                     next_states.append(data[3])
-                    dones.append(data[4])
+                    dones      .append(data[4])
 
                 q_network_params, opt_state, loss, new_priorities = train_step(
                     q_network_params,
@@ -265,8 +259,7 @@ try:
                         jnp.asarray(rewards, dtype=jnp.float32),
                         jnp.asarray(next_states),
                         jnp.asarray(dones, dtype=jnp.float32)
-                    ),
-                    train_key
+                    )
                 )
                 for i in range(batch_size):
                     idx = batch[i][0]
